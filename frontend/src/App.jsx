7d3194a5c274e58a80903d863e82b34fa8b6bdcd
stenkrk <stenkrk@tlu.ee>
2026-06-09 14:07:01 +0300

    avaleht viib log-in lehele

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,32 +4,37 @@ import { doc, setDoc } from "firebase/firestore";
 import { auth, googleProvider, db } from "./firebaseConfig";
 import HomePage from './pages/HomePage'
 import './App.css'
-import { Link, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-
+function LoginPage() {
   const loginWithGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
       const user = result.user;
-
       await setDoc(doc(db, "users", user.uid), {
         name: user.displayName,
         email: user.email,
         lastLogin: new Date()
       });
-
       alert(`Welcome ${user.displayName}`);
     } catch (error) {
       alert(error.message);
     }
   };
-
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Logi sisse</h1>
       <button onClick={loginWithGoogle}>Logi sisse Googlega</button>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
