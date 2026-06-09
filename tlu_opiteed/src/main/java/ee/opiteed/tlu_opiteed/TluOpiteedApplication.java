ad6c765ece7d688694479348a4270a64adbddfd2
ukukurm <ukukurm@tlu.ee>
2026-06-09 11:36:35 +0300

    algne setup \ claude.md täitmine \ spring-boot working

---
+++ tlu_opiteed/src/main/java/ee/opiteed/tlu_opiteed/TluOpiteedApplication.java
@@ -0,0 +1,13 @@
+package ee.opiteed.tlu_opiteed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TluOpiteedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TluOpiteedApplication.class, args);
+	}
+
+}
---
+++ tlu_opiteed/src/test/java/ee/opiteed/tlu_opiteed/TluOpiteedApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.opiteed.tlu_opiteed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TluOpiteedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
